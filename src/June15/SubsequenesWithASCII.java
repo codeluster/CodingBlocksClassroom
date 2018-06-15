c92baa1242e2dc190a09014f3b3341f5e3da7a99
codeluster <tanmaysingal@icloud.com>
2018-06-15 16:28:08 +0530

    Subsequences with ASCII solved

--- src/June15/SubsequenesWithASCII.java
+++ src/June15/SubsequenesWithASCII.java
@@ -1,8 +1,38 @@
 package June15;
 
+import java.util.ArrayList;
+
 public class SubsequenesWithASCII {
 
-    public static void main(){
+    public static void main(String[] args) {
+
+        System.out.println(getSS("abc"));
+
+    }
+
+
+    private static ArrayList<String> getSS(String str) {
+
+        ArrayList<String> myResult = new ArrayList<>();
+
+        if (str.length() == 0) {
+            myResult.add("");
+            return myResult;
+        }
+
+        char setAside = str.charAt(0);
+
+        ArrayList<String> recursionResult = getSS(str.substring(1));
+
+        for (String x : recursionResult) {
+
+            myResult.add(x);
+            myResult.add(setAside + x);
+            myResult.add((int)setAside + x);
+
+        }
+
+        return myResult;
 
     }
 
