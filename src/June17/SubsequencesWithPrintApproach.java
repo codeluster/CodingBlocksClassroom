ad19f89a93fe6b50bfee601463587f347123b74d
codeluster <tanmaysingal@icloud.com>
2018-06-17 15:57:40 +0530

    Subsequences with Print Approach solved

--- src/June17/SubsequencesWithPrintApproach.java
+++ src/June17/SubsequencesWithPrintApproach.java
@@ -4,12 +4,24 @@ package June17;
 
 public class SubsequencesWithPrintApproach {
 
-    public static void main(String[] sfji){
+    public static void main(String[] sfji) {
 
+        getSubSequences("abcd", "");
 
     }
 
-    public static void getSubSequences(String question, String answer){
+    public static void getSubSequences(String question, String answer) {
+
+        if (question.equals("")) {
+            System.out.println(answer);
+            return;
+        }
+
+        char first = question.charAt(0);
+        question = question.substring(1);
+
+        getSubSequences(question, answer);
+        getSubSequences(question, first + answer);
 
     }
 
