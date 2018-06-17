2df74cf1a88e3e360fd8c690a7fc08c87d05e0df
codeluster <tanmaysingal@icloud.com>
2018-06-17 16:51:48 +0530

    Board and Maze Path with Print Approach solved

---
+++ src/June17/BoardPathWithPrintApproach.java
@@ -0,0 +1,30 @@
+package June17;
+
+public class BoardPathWithPrintApproach {
+
+    public static void main(String[] strings) {
+
+        System.out.print(getBoardPath(0, 10, ""));
+
+
+    }
+
+    private static int getBoardPath(int curr, int end, String ans) {
+
+        if (curr == end) {
+            System.out.println(ans);
+            return 1;
+        } else if (curr > end) {
+            return 0;
+        } else {
+
+            int count = 0;
+
+            for (int dice = 1; dice <= 6; dice++) {
+                count += getBoardPath(curr + dice, end, ans + dice);
+            }
+            return count;
+        }
+    }
+
+}
---
+++ src/June17/MazePathWithPrintApproach.java
@@ -0,0 +1,26 @@
+package June17;
+
+public class MazePathWithPrintApproach {
+
+    public static void main(String[] args) {
+
+        getPath(0, 0, 2, 2, "");
+
+    }
+
+    private static int getPath(int cr, int cc, int er, int ec, String ans) {
+
+        if (cr == er && cc == ec) {
+            System.out.println(ans);
+            return 1;
+        } else if (cr > er || cc > ec) {
+            return 0;
+        } else {
+            int hc = getPath(cr, cc + 1, er, ec, "V" + ans);
+            int vc = getPath(cr + 1, cc, er, ec, "H" + ans);
+            return hc + vc;
+        }
+
+    }
+
+}
