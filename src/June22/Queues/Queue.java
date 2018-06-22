3b1c5cef952227be37026f370e0c943f951774ec
codeluster <tanmaysingal@icloud.com>
2018-06-22 17:17:10 +0530

    built custom queue

--- src/June22/Queues/Queue.java
+++ src/June22/Queues/Queue.java
@@ -19,48 +19,33 @@ public class Queue {
     }
 
     public void enqueue(int item) throws Exception {
-
         if (size == data.length) {
             throw new Exception("The queue is full!");
         }
-
         data[(this.front + size) % data.length] = item;
         size++;
     }
 
     public int dequeue() throws Exception {
-
-        if (size == 0) {
-            throw new Exception("The queue is empty!");
-        }
-
+        if (size == 0) throw new Exception("The queue is empty!");
         int returnValue = data[this.front];
-        this.front++;
+        front = (this.front + 1) % data.length;
         size--;
         return returnValue;
-
     }
 
     public int getFront() throws Exception {
-        if (size == 0) {
-            throw new Exception("The queue is empty!");
-        }
+        if (size == 0) throw new Exception("The queue is empty!");
         return data[this.front];
     }
 
     public void display() {
-
         System.out.println("--------------");
-
         for (int i = 0; i < size; i++) {
-
             System.out.print(data[(i + this.front) % this.data.length] + " ");
-
         }
-
         System.out.println();
         System.out.println("--------------");
-
     }
 
 }
