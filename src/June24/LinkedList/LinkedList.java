152f01ec5c5510bcef8d58cffa7026f8e477d2fb
Tanmay <tanmaysingal@icloud.com>
2018-06-26 14:49:36 +0530

    Solved linkedlist reverse...

--- src/June24/LinkedList/LinkedList.java
+++ src/June24/LinkedList/LinkedList.java
@@ -23,6 +23,10 @@ public class LinkedList {
         return this.tail.data;
     }
 
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
     public int getSize() {
         return this.size;
     }
@@ -30,6 +34,8 @@ public class LinkedList {
     public int getItemAt(int index) throws Exception {
         if (this.size == 0) throw new Exception("The Linked List is Empty!");
         if (index < 0 || index >= this.size) throw new Exception("This index does not exist!");
+        if (index == 0) return this.getFirst();
+        if (index == this.size - 1) return this.getLast();
         Node temp = head;
         for (int i = 0; i < index; i++) {
             temp = temp.next;
@@ -62,6 +68,192 @@ public class LinkedList {
 
         System.out.println();
         System.out.println("------------");
+
+    }
+
+    public void RDI() throws Exception {
+
+        int left = 0;
+        int right = this.size - 1;
+
+        while (left < right) {
+
+            Node ln = getNodeAt(left);
+            Node rn = getNodeAt(right);
+
+            int temp = ln.data;
+            ln.data = rn.data;
+            rn.data = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public int getMideItem() {
+
+        Node slow = this.head;
+        Node fast = this.head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow.data;
+
+    }
+
+    public int getFromLast(int index) {
+
+        Node slow = this.head;
+        Node fast = this.head;
+
+        //Move fast forward by index steps
+        for (int i = 0; i < index; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow.data;
+
+    }
+
+    public void kReverse(int k) throws Exception {
+
+        if (this.isEmpty()) throw new Exception("The Linked List is Empty!");
+
+        LinkedList prev = null;
+        LinkedList curr = null;
+
+        while (this.size != 0) {
+
+            curr = new LinkedList();
+
+            for (int i = 0; i < k; i++) {
+                curr.addFirst(this.removeFirst());
+
+            }
+
+            if (prev == null) {
+                prev = curr;
+            } else {
+                prev.tail.next = curr.head;
+                prev.tail = curr.tail;
+                prev.size += curr.size;
+            }
+
+        }
+
+        this.head = prev.head;
+        this.tail = prev.tail;
+        this.size = prev.size;
+
+    }
+
+    public void RPI() {
+
+        Node prev = this.head;
+        Node curr = this.head.next;
+
+        while (curr != null) {
+
+            Node ahead = curr.next;
+            curr.next = prev;
+
+            prev = curr;
+            curr = ahead;
+
+        }
+
+        Node temp = this.head;
+        this.head = this.tail;
+        this.tail = temp;
+
+        this.tail.next = null;
+
+    }
+
+    public void RPR() {
+        RPR(this.head, this.head.next);
+
+        Node temp = this.head;
+        this.head = this.tail;
+        this.tail = temp;
+
+        this.tail.next = null;
+
+    }
+
+    private void RPR(Node prev, Node curr) {
+
+        if (curr == null)
+            return;
+
+        RPR(curr, curr.next);
+
+        curr.next = prev;
+    }
+
+    private class Mover {
+        Node left;
+    }
+
+    public void RDR() {
+        Mover mover = new Mover();
+        mover.left = this.head;
+
+        RDR(mover, this.head, 0);
+    }
+
+    private void RDR(Mover mover, Node right, int count) {
+
+        if (right == null) {
+            return;
+        }
+
+        RDR(mover, right.next, count + 1);
+
+        if (count >= this.size / 2) {
+            int temp = mover.left.data;
+            mover.left.data = right.data;
+            right.data = temp;
+        }
+        mover.left = mover.left.next;
+
+    }
+
+    public void fold() {
+
+        Mover mover = new Mover();
+        mover.left = this.head;
+
+        fold(mover, this.head, 0);
+    }
+
+    private void fold(Mover mover, Node right, int count) {
+
+        if (right == null) {
+            return;
+        }
+
+        fold(mover, right.next, count + 1);
+
+        if (count > size / 2) {
+            Node temp = mover.left.next;
+            mover.left.next = right;
+            right.next = temp;
+            mover.left = temp;
+        }
+        if (count == size / 2) {
+
+            this.tail = right;
+            this.tail.next = null;
+        }
 
     }
 
---
+++ src/June26/LinkedListClient.java
@@ -0,0 +1,26 @@
+package June26;
+
+import June24.LinkedList.LinkedList;
+
+public class LinkedListClient {
+
+    public static void main(String[] args) throws Exception {
+
+        LinkedList linkedList = new LinkedList();
+
+        linkedList.addLast(10);
+        linkedList.addLast(20);
+        linkedList.addLast(30);
+        linkedList.addLast(40);
+        linkedList.addLast(50);
+        linkedList.addLast(60);
+        linkedList.addLast(70);
+        linkedList.addLast(80);
+        linkedList.addLast(90);
+        linkedList.display();
+        linkedList.kReverse(3);
+        linkedList.display();
+
+    }
+
+}
