8dc6ae55d0e586e9a69a997b3586d854feb847a4
tanmay <tanmaysingal@icloud.com>
2018-06-28 19:59:09 +0530

    Built getHeight() and turned off all prompts while creating a new tree if a string of pre written responses is passed into the scanner

--- src/June27/OnlineLecture/BinaryTree/BinaryTreeClient.java
+++ src/June27/OnlineLecture/BinaryTree/BinaryTreeClient.java
@@ -4,6 +4,7 @@ public class BinaryTreeClient {
 
     public static void main(String[] args) {
 
+        // For testing purposes only
         String dummyTree = "50 true 25 true 38 false true 48 true 10 false false true 45 true 85 false true 60 false";
 
         BinaryTree binaryTree = new BinaryTree(dummyTree);
